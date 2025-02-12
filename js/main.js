be198319e3dbad8e6a8fece4d0f27e48c4a4e42e
KarimOsama99 <ko5418301@gmail.com>
2025-02-12 20:09:17 +0200

    DEMO

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
 (function ($) {
   ("use strict");
 
+  // DARK THEME
+    const darkTheme = "dark-theme";
+    const iconMoon = "fad fa-eclipse-alt";
+    const iconSun = "fad fa-sun";
+
+    // تحميل الثيم المحفوظ
+    if (localStorage.getItem("theme") === "dark") {
+      $("body").addClass(darkTheme);
+      $("#theme-button svg").removeClass(iconMoon).addClass(iconSun);
+    }
+
+    // عند الضغط على الزر
+    $("#theme-button").click(function (e) {
+      e.preventDefault();
+
+      $("body").toggleClass(darkTheme);
+      $("#theme-button svg").toggleClass(`${iconMoon} ${iconSun}`);
+
+      // حفظ الثيم الحالي
+      localStorage.setItem(
+        "theme",
+        $("body").hasClass(darkTheme) ? "dark" : "light"
+      );
+    });
+
+  // Preloader
+
   $(window).on("load", function (event) {
     $("#preloader").delay(500).fadeOut(500);
   });
